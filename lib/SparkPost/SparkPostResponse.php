9b320a1d2e704acf505cb34dbc5485b6de0623f5
virtualLast <mjlast@gmail.com>
2026-02-12 15:50:58 +0000

    updated return types and new decoded method

--- lib/SparkPost/SparkPostResponse.php
+++ lib/SparkPost/SparkPostResponse.php
@@ -2,6 +2,7 @@
 
 namespace SparkPost;
 
+use Psr\Http\Message\MessageInterface as MessageInterface;
 use Psr\Http\Message\ResponseInterface as ResponseInterface;
 use Psr\Http\Message\StreamInterface as StreamInterface;
 
@@ -39,84 +40,90 @@ class SparkPostResponse implements ResponseInterface
     }
 
     /**
-     * Returns the body.
-     *
-     * @return array $body - the json decoded body from the http response
+     * Returns the body as StreamInterface (PSR-7).
      */
-    public function getBody()
+    public function getBody(): StreamInterface
     {
-        $body = $this->response->getBody();
-        $body_string = $body->__toString();
+        return $this->response->getBody();
+    }
 
+    /**
+     * Returns the json decoded body from the http response.
+     *
+     * @return array $body
+     */
+    public function getBodyDecoded(): array
+    {
+        $body_string = $this->response->getBody()->__toString();
         $json = json_decode($body_string, true);
 
-        return $json;
+        return $json ?? [];
     }
 
     /**
      * pass these down to the response given in the constructor.
      */
-    public function getProtocolVersion()
+    public function getProtocolVersion(): string
     {
         return $this->response->getProtocolVersion();
     }
 
-    public function withProtocolVersion($version)
+    public function withProtocolVersion(string $version): MessageInterface
     {
         return $this->response->withProtocolVersion($version);
     }
 
-    public function getHeaders()
+    public function getHeaders(): array
     {
         return $this->response->getHeaders();
     }
 
-    public function hasHeader($name)
+    public function hasHeader(string $name): bool
     {
         return $this->response->hasHeader($name);
     }
 
-    public function getHeader($name)
+    public function getHeader(string $name): array
     {
         return $this->response->getHeader($name);
     }
 
-    public function getHeaderLine($name)
+    public function getHeaderLine(string $name): string
     {
         return $this->response->getHeaderLine($name);
     }
 
-    public function withHeader($name, $value)
+    public function withHeader(string $name, $value): MessageInterface
     {
         return $this->response->withHeader($name, $value);
     }
 
-    public function withAddedHeader($name, $value)
+    public function withAddedHeader(string $name, $value): MessageInterface
     {
         return $this->response->withAddedHeader($name, $value);
     }
 
-    public function withoutHeader($name)
+    public function withoutHeader(string $name): MessageInterface
     {
         return $this->response->withoutHeader($name);
     }
 
-    public function withBody(StreamInterface $body)
+    public function withBody(StreamInterface $body): MessageInterface
     {
         return $this->response->withBody($body);
     }
 
-    public function getStatusCode()
+    public function getStatusCode(): int
     {
         return $this->response->getStatusCode();
     }
 
-    public function withStatus($code, $reasonPhrase = '')
+    public function withStatus(int $code, string $reasonPhrase = ''): ResponseInterface
     {
         return $this->response->withStatus($code, $reasonPhrase);
     }
 
-    public function getReasonPhrase()
+    public function getReasonPhrase(): string
     {
         return $this->response->getReasonPhrase();
     }
